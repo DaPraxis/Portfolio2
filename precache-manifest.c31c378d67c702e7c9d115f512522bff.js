4d5bbda9161b2b3fd56b9ada844ad37d5935b92c
DaPraxis <haoyanjiang0709@gmail.com>
2019-11-27 13:40:22 -0500

    Updates

---
+++ precache-manifest.c31c378d67c702e7c9d115f512522bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c794ea2fbd33de8ad60588a48c36deb",
+    "url": "/Portfolio2/index.html"
+  },
+  {
+    "revision": "44a2c4540ffdd4e2de2b",
+    "url": "/Portfolio2/static/css/2.7b128773.chunk.css"
+  },
+  {
+    "revision": "cb708fd7866b1f179c1e",
+    "url": "/Portfolio2/static/css/main.03fadf40.chunk.css"
+  },
+  {
+    "revision": "44a2c4540ffdd4e2de2b",
+    "url": "/Portfolio2/static/js/2.982b2f45.chunk.js"
+  },
+  {
+    "revision": "cb708fd7866b1f179c1e",
+    "url": "/Portfolio2/static/js/main.9363c5bd.chunk.js"
+  },
+  {
+    "revision": "11e8ab635c536dd5ff88",
+    "url": "/Portfolio2/static/js/runtime-main.2b74f42c.js"
+  }
+]);
